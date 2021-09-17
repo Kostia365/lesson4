76c4bd65ace8daf1320d7326ff5481f6fec7f0bd
Костя <knilok37@gmail.com>
2021-09-17 22:16:52 +0300

    jb

--- src/test.java
+++ src/test.java
@@ -1,7 +1,14 @@
 import java.util.Scanner;
 public class test {
     public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int number = scanner.nextInt();
 
+    if (number % 2 == 1) {
+        System.out.println("введённое число не чётное");
+    } else {
+        System.out.println("введённое число чётное");
+    }
     }
 
 }
