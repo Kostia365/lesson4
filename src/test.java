116aac927d3e75d0dcce762b9d8a8c2e27e1306d
Костя <knilok37@gmail.com>
2021-09-17 22:25:23 +0300

    N9-10

--- src/test.java
+++ src/test.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 public class test {
     public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    int number = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
 
-    if (number % 2 == 1) {
-        System.out.println("введённое число не чётное");
-    } else {
-        System.out.println("введённое число чётное");
-    }
+        if (number % 2 == 1) {
+            System.out.println("введённое число не чётное");
+        } else {
+            System.out.println("введённое число чётное");
+        }
     }
 
 }
