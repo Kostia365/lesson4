bd38290690bb5d9cfefd4d7d4e78d5cfab0995b5
Костя <knilok37@gmail.com>
2021-11-13 21:41:17 +0300

    55253YU

---
+++ src/hometaskformirik.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class hometaskformirik {
+    class Solution {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            int num = scanner.nextInt();
+            int num3 = scanner.nextInt();
+            int num1[] = ;
+            System.out.print(num1);
+        }
+    }
+}
